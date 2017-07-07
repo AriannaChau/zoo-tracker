bde08d0b0484b6bc80dfc5abfaf2e324c69874a9
Arianna Chau <arianna@ariannas-mbp.lan>
2017-07-07 09:40:03 -0700

    Create skeleton for project.

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Zoo Tracker</h1>
+  `
+})
+
+export class AppComponent {
+
+
+}
